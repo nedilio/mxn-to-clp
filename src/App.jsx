b4d455989ebe247a8c51e4b9aee85f18ce27a100
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-09-29 03:02:31 -0300

    feat: agregando cambio de paises para convertir

--- src/App.jsx
+++ src/App.jsx
@@ -3,65 +3,116 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [dolarChile, setDolarChile] = useState(0);
   const [dolarMX, setDolarMX] = useState(0);
+  const [dolarARS, setDolarARS] = useState(0);
+
+  const mexico = { id: "MXN", flag: "🇲🇽", dolar: dolarMX };
+  const argentina = { id: "ARS", flag: "🇦🇷", dolar: dolarARS };
+
+  const [dolarChile, setDolarChile] = useState(0);
   const [eurMX, setEurMX] = useState(0);
   const [clp, setClp] = useState(0);
   const [usd, setUsd] = useState(0);
   const [eur, setEur] = useState(0);
-  const [mxn, setMxn] = useState(0);
+  const [input, setInput] = useState(0);
+  const [country, setCountry] = useState(argentina);
 
   const handleOnChange = (e) => {
-    let mxInput = 0;
+    let input = 0;
     if (e.target.value === "") {
-      mxInput = 0;
+      input = 0;
     } else {
-      mxInput = parseInt(e.target.value);
+      input = parseInt(e.target.value);
     }
-    setUsd(mxInput / dolarMX);
-    setClp((mxInput / dolarMX) * dolarChile);
-    setEur(mxInput/eurMX);
-    setMxn(mxInput);
+    setInput(input);
   };
 
-  useEffect(() => {
+  const handleChangeCountry = (event, country) => {
+    // setDolarArSsetDolarArS(rate);
+    setCountry(countries[country]);
+    const active = document
+      .querySelector("li.active")
+      .classList.remove("active");
+    event.target.classList.add("active");
+  };
 
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': 'f5b8bf79e4msh25787ede94720aep17173ejsne2e841f052f2',
-          'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-        }
-      };
-      
-      fetch('https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=MXN&q=1.0', options)
-        .then(response => response.json())
-        .then(response => setDolarMX(response))
-        .catch(err => console.error(err));
-        
-        fetch('https://currency-exchange.p.rapidapi.com/exchange?from=EUR&to=MXN&q=1', options)
-        .then(response => response.json())
-        .then(response => setEurMX(response))
-        .catch(err => console.error(err));
+  const countries = { mx: mexico, ar: argentina };
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "f5b8bf79e4msh25787ede94720aep17173ejsne2e841f052f2",
+        "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      "https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=MXN&q=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setDolarMX(response.toFixed(2)))
+      .catch((err) => console.error(err));
+
+    fetch(
+      "https://currency-exchange.p.rapidapi.com/exchange?from=EUR&to=MXN&q=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setEurMX(response))
+      .catch((err) => console.error(err));
 
     fetch(
       "https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar?apikey=74480963fcc3674c7781f739601f8dcee31aef6b&formato=json"
     )
       .then((res) => res.json())
       .then((res) => setDolarChile(parseInt(res.Dolares[0].Valor)));
+
+    fetch("https://api.bluelytics.com.ar/v2/latest")
+      .then((res) => res.json())
+      .then((res) => setDolarARS(res.blue.value_buy));
   }, []);
+
+  useEffect(() => {
+    console.log("cambia input");
+    console.log(country.dolar);
+    if (country.dolar) {
+      setUsd(input / country.dolar);
+      setClp((input / country.dolar) * dolarChile);
+      setEur(input / eurMX);
+    }
+  }, [input, country]);
+  useEffect(() => {
+    setCountry(mexico);
+    console.log(countries);
+  }, [dolarMX, eurMX]);
 
   return (
     <div className="App">
       <nav>
         <ul>
-          <li id="blue">
-            1💲 = {dolarMX.toFixed(2)} MXN
+          <li
+            id="mxn"
+            className="active"
+            onClick={() => handleChangeCountry(event, "mx")}
+          >
+            🇲🇽
+          </li>
+          <li
+            id="ars"
+            className=""
+            onClick={() => handleChangeCountry(event, "ar")}
+          >
+            🇦🇷
           </li>
         </ul>
       </nav>
-      <h1>Convertir 🌮 🔁 🌶</h1>
-      <label htmlFor="ars">Precio en MXN</label>
+      <h1>Convertir {country.flag} 🔁 🇨🇱</h1>
+      <h5 id="blue">
+        1💲 = {country.dolar || dolarMX} {country.id}
+      </h5>
+      <label htmlFor="ars">Precio en {country.id}</label>
       <input
         type="number"
         inputMode="decimal"
@@ -72,8 +123,8 @@ function App() {
         <h2>🇨🇱 CLP: {clp.toFixed(0)}</h2>
         <p>1💲 = {dolarChile} CLP</p>
       </div>
-      <h2>💵USD: {usd.toFixed(2)}</h2>
-      <h2>💶 EUR: {eur.toFixed(2)}</h2>
+      <h2>🇺🇸 USD: {usd.toFixed(2)}</h2>
+      {country.id === "MXN" && <h2>🇪🇺 EUR: {eur.toFixed(2)}</h2>}
       <footer>
         <a
           href="https://github.com/nedilio"
